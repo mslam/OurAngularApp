a37f1982d341a1110d629be18d5006c3963d8d30
Jeff Obadal <Jeffrey-Obadal@UIowa.edu>
2017-11-05 21:17:04 -0600

    added Angular2TokenService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {Angular2TokenService} from 'angular2-token';
+import {HttpModule} from '@angular/http';
+import {RouterModule} from '@angular/router';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
+    RouterModule
   ],
-  providers: [],
+  providers: [ Angular2TokenService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
   production: true
+
+  // Todo: add correct URL for production/staging.
+  // token_auth_config: {
+  //   apiBase: 'http:// '
+  // }
 };
