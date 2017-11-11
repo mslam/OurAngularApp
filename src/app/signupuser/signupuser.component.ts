bb2ca60fb02e7c5bd6ff547bbb3d99412816de77
Mohammad Shafkat Islam <mohammadshafkat-islam@uiowa.edu>
2017-11-11 13:24:32 -0600

    Defined the login page for existing users.

---
+++ src/app/signupuser/signupuser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signupuser',
+  templateUrl: './signupuser.component.html',
+  styleUrls: ['./signupuser.component.css']
+})
+export class SignupuserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
