d8ef28aed2517f418561f054df7e96354bffb0c3
Mohammad Shafkat Islam <mohammadshafkat-islam@uiowa.edu>
2017-11-17 17:48:24 -0600

    Status: Login Page ready, Issue: Need to fix the uncaught in promise issue.

---
+++ src/app/shared/loginuser/loginuser.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import {Loginuser} from "../loginuser";
+import {Http, Headers} from "@angular/http";
+import 'rxjs/add/operator/toPromise';
+import {AlertService} from "../alert/alert.service";
+
+@Injectable()
+export class LoginuserService {
+
+  constructor(private http: Http,
+              public alertService: AlertService) { }
+  private url = 'http://localhost:3000/loginuser';  // URL to web api
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  createLoginuser(loginuser : Loginuser): Promise<Loginuser> {
+
+    console.log(loginuser);
+
+    return this.http
+          .post(this.url, JSON.stringify(loginuser), {headers: this.headers})
+          .toPromise()
+          .then(response => response.json() as Loginuser)
+          .catch(response => {
+            var message = response.json().message;
+            console.log(response.json().message);
+            this.alertService.display('danger', message);
+            return Promise.reject(message);
+          });
+  }
+}
