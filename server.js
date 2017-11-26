1592b0ec1c36bdeb4388c4821133f73d4937618b
Jeff Obadal <Jeffrey-Obadal@UIowa.edu>
2017-11-26 15:18:35 -0600

    WIP: configured express server

--- server.js
+++ server.js
@@ -2,6 +2,19 @@ const express = require('express');
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-express()
-  .use(express.static(path.join(__dirname, 'dist')))
-  .listen(PORT);
+const app = express();
+
+app.set('port', PORT);
+
+
+// set root directory for server
+app.use(express.static(path.join(__dirname, 'dist')));
+
+//redirect all requests to index
+app.get('*', function(req,res){
+  res.sendfile(path.join(__dirname, '/dist/index.html'))
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
