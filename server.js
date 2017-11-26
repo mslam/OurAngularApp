5b45f23333b7715d06b1cf09b7be5bb4365cd8c1
Jeff Obadal <Jeffrey-Obadal@UIowa.edu>
2017-11-26 15:31:32 -0600

    WIP: Added forced https connection

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// Force HTTPS
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+};
+
+app.use(forceSSL());
+
 app.set('port', PORT);
 
 
